5bb3aee753b741a7b99189939318f0eb294f2f09
Embersands game Studios <eitan.levy.159@gmail.com>
2020-08-02 11:45:57 +0300

    Add doors

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    #region variables
+    public Switch @switch;
+    public Switch.SwitchState switchState;
+
+    BoxCollider2D boxCollider;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (switchState == @switch.state)
+        {
+            // Door is open
+            boxCollider.isTrigger = true;
+
+            // Add aniamtion?
+            // Color change is temporary
+            GetComponent<SpriteRenderer>().color = Color.green;
+        }
+        else
+        {
+            boxCollider.isTrigger = false;
+
+            GetComponent<SpriteRenderer>().color = Color.red;
+        }
+    }
+}
