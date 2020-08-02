bcdffae55d59c9d379dc0b57aa56c928ccd28976
g33king10 <turhuhxckuo@gmail.com>
2020-08-02 17:31:58 +0300

    Pressure Plate Fixed so it will not recognize held boxes

--- Assets/Scripts/Plate.cs
+++ Assets/Scripts/Plate.cs
@@ -1,20 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class Plate : Trigger
 {
+
+    public Box box;
+    public bool isTouchingBox = false;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Box box = collision.GetComponent<Box>();
-        if (box != null) 
-            state = TriggerState.On;
+        Box enterBox = collision.GetComponent<Box>();
+        if (enterBox != null)
+        {
+            box = enterBox;
+            isTouchingBox = true;
+        }
+    }
+
+    public void Update()
+    {
+        if (box == null) return;
+        if (isTouchingBox && !box.isHeld) state = TriggerState.On;
+        else state = TriggerState.Off;
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        Box box = collision.GetComponent<Box>();
-        if (box != null) 
-            state = TriggerState.Off;
+        Box exitBox = collision.GetComponent<Box>();
+        if (exitBox != null)
+            isTouchingBox = false;
+        if (exitBox == box) box = null;
     }
 }
