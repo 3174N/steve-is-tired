dd18800ddce198e0d3893bb014ec9470bf00a0f8
Embersands game Studios <eitan.levy.159@gmail.com>
2020-08-02 10:27:26 +0300

    Add player movement

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerController : MonoBehaviour
+{
+    #region variables
+    public float speed;
+
+    float horizontal, vertical;
+    Vector2 movement;
+
+    Rigidbody2D rb;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+
+        movement.Set(horizontal, vertical);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+    }
+}
