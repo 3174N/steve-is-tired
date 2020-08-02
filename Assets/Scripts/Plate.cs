422321e284f57444d6ce1a4e5a415c42499baa71
Embersands game Studios <eitan.levy.159@gmail.com>
2020-08-02 15:02:07 +0300

    Fix pressure plate

--- Assets/Scripts/Plate.cs
+++ Assets/Scripts/Plate.cs
@@ -7,12 +7,14 @@ public class Plate : Trigger
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Box box = collision.GetComponent<Box>();
-        if (box != null && !box.isHeld) state = TriggerState.On;
+        if (box != null) 
+            state = TriggerState.On;
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         Box box = collision.GetComponent<Box>();
-        if (box != null) state = TriggerState.Off;
+        if (box != null) 
+            state = TriggerState.Off;
     }
 }
