248c2ef8f030e1c056eb34cc6fc46532244ab1c9
Embersands game Studios <eitan.levy.159@gmail.com>
2020-08-02 10:55:48 +0300

    Add rewind

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,12 @@ public class PlayerController : MonoBehaviour
     #region variables
     public float speed;
 
+    public KeyCode rewindKey = KeyCode.R;
+    public float maxRewindTime;
+    float rewindTime;
+    bool isRewinding;
+    List<Vector2> positions;
+
     float horizontal, vertical;
     Vector2 movement;
 
@@ -16,19 +22,77 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        positions = new List<Vector2>();
+
+        rewindTime = maxRewindTime;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Movement
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
 
         movement.Set(horizontal, vertical);
+
+        // Rewind
+        if (Input.GetKeyDown(rewindKey))
+            StartRewind();
+        if (Input.GetKeyUp(rewindKey))
+            StopRewind();
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+
+        if (isRewinding)
+            Rewind();
+        else
+            Record();
     }
+
+    #region rewind
+    void Record()
+    {
+        if (rewindTime <= 0)
+            return;
+
+        if (positions.Count > Mathf.Round((1 / Time.fixedDeltaTime) * rewindTime))
+        {
+            positions.RemoveAt(positions.Count - 1);
+        }
+
+        positions.Insert(0, rb.position);
+    }
+
+    void Rewind()
+    {
+        if (positions.Count > 0)
+        {
+            rb.MovePosition(positions[0]);
+            positions.RemoveAt(0);
+
+            rewindTime -= Time.fixedDeltaTime;
+        }
+        else
+        {
+            StopRewind();
+        }
+    }
+
+    public void StartRewind()
+    {
+        isRewinding = true;
+        rb.isKinematic = true;
+    }
+
+    public void StopRewind()
+    {
+        isRewinding = false;
+        rb.isKinematic = false;
+    }
+    #endregion
 }
