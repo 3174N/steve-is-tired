6b3c35a089192628bc1fbb0a8df68f3a0ee1958e
Embersands game Studios <eitan.levy.159@gmail.com>
2020-08-02 11:36:37 +0300

    Add switches

---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    #region variables
+    public enum SwitchState
+    {
+        On,
+        Off
+    }
+    public SwitchState state = SwitchState.Off;
+
+    bool playerIsIn;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerIsIn)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("Yay");
+                if (state == SwitchState.Off)
+                    state = SwitchState.On;
+                else
+                    state = SwitchState.Off;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerController player = collision.GetComponent<PlayerController>();
+        if (player != null)
+        {
+            playerIsIn = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        playerIsIn = false;
+    }
+}
