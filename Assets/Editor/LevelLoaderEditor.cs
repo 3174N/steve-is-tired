0482df4a277fb3ae11be5cb0d4d9d36c50508866
Embersands game Studios <eitan.levy.159@gmail.com>
2020-08-03 12:01:24 +0300

    Add level loader

---
+++ Assets/Editor/LevelLoaderEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LevelLoader))]
+public class LevelLoaderEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        LevelLoader loader = (LevelLoader)target;
+
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("Load Next Level"))
+        {
+            loader.LoadNextLevel();
+        }
+        GUILayout.EndHorizontal();
+    }
+}
---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class LevelLoader : MonoBehaviour
+{
+    #region variables
+    public float transitionTime = 1;
+
+    public Slider slider;
+    public TextMeshProUGUI progressText;
+
+    Animator transition;
+    #endregion
+
+    private void Awake()
+    {
+        transition = gameObject.GetComponent<Animator>();
+    }
+
+    public void Load(int levelIndex)
+    {
+        StartCoroutine(LoadLevel(levelIndex));
+    }
+
+    public void Load(string levelIndex)
+    {
+        StartCoroutine(LoadLevel(levelIndex));
+    }
+    
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    public void LoadStart()
+    {
+        StartCoroutine(LoadLevel(1));
+    }
+
+    public void LoadMenu()
+    {
+        StartCoroutine(LoadLevel(0));
+    }
+
+    public void LoadCurrentLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        AsyncOperation operation = SceneManager.LoadSceneAsync(levelIndex);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            slider.value = progress;
+            progressText.text = progress * 100 + "%";
+
+            yield return null;
+        }
+    }
+
+    IEnumerator LoadLevel(string levelIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        AsyncOperation operation = SceneManager.LoadSceneAsync(levelIndex);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            slider.value = progress;
+            progressText.text = progress * 100 + "%";
+
+            yield return null;
+        }
+    }
+}
