066c56d35d418f707eb1b6a4990daa2b4f8382cd
Embersands game Studios <eitan.levy.159@gmail.com>
2020-08-03 18:08:24 +0300

    Add audio manager

---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume = 1;
+    [Range(0.1f, 3f)]
+    public float pitch = 1;
+
+    public bool loop;
+
+    public enum SoundType
+    {
+        SFX,
+        Music
+    }
+    public SoundType type = SoundType.SFX;
+
+    [HideInInspector]
+    public AudioSource source;
+}
