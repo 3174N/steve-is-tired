8ab17a37487f79dfce39deafe4bcf7e3311db966
Embersands game Studios <eitan.levy.159@gmail.com>
2020-08-04 16:29:38 +0300

    Add pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    #region variables
+    public GameObject pauseMenu;
+
+    public KeyCode pauseKey = KeyCode.Escape;
+
+    bool isPaused;
+    #endregion
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(pauseKey))
+        {
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void Restart()
+    {
+        Resume();
+        FindObjectOfType<LevelLoader>().LoadCurrentLevel();
+    }
+
+    public void Menu()
+    {
+        Resume();
+        FindObjectOfType<LevelLoader>().LoadMenu();
+    }
+}
