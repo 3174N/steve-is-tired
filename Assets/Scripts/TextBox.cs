c6d9c39603e042592eec87f249f897d5114f0131
Embersands game Studios <eitan.levy.159@gmail.com>
2020-08-04 17:52:59 +0300

    Add textbox

--- Assets/Scripts/TextBox.cs
+++ Assets/Scripts/TextBox.cs
@@ -1,20 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TextBox : MonoBehaviour
 {
     #region variables
+    public GameObject canvas;
+    public TextMeshProUGUI text;
+
     public TextItem[] texts;
 
-    bool hasShown;
+    bool hasShown = false;
+
+    float duration;
+
+    Animator animator;
     #endregion
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (!hasShown)
+        if (hasShown)
+            return;
+
+        StartCoroutine(Display());
+
+        hasShown = true;
+    }
+
+    IEnumerator Display()
+    {
+        foreach (TextItem textItem in texts)
         {
-            hasShown = true;
+            canvas.SetActive(true);
+            animator.SetTrigger("Show");
+
+            text.text = textItem.text;
+            text.color = textItem.color;
+
+            yield return new WaitForSeconds(textItem.duration);
+
+            animator.SetTrigger("Fade");
+            yield return new WaitForSeconds(1f);
         }
+
+        canvas.SetActive(false);
     }
 }
