f78cc7226a49466b46ff0c989b21a6fc08fffd15
Embersands game Studios <eitan.levy.159@gmail.com>
2020-08-04 17:12:33 +0300

    Add placeholder

---
+++ Assets/Scripts/TextBox.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextBox : MonoBehaviour
+{
+    #region variables
+    public TextItem[] texts;
+
+    bool hasShown;
+    #endregion
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (!hasShown)
+        {
+            hasShown = true;
+        }
+    }
+}
