228234c6f4bf7705540e7d0259eca1b688f82513
Embersands game Studios <eitan.levy.159@gmail.com>
2020-08-06 09:58:59 +0300

    Updated vending machine

--- Assets/Scripts/EnergyDrink.cs
+++ Assets/Scripts/EnergyDrink.cs
@@ -19,7 +19,7 @@ public class EnergyDrink : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         PlayerController player = collision.GetComponent<PlayerController>();
-        if (player != null && player.maxRewindTime > player.rewindTime && !player.infiniteRewind)
+        if (player != null && player.maxRewindTime > player.rewindTime)
         {
             player.ResetRewindTime();
             FindObjectOfType<AudioManager>().Play("Energy Drink");
---
+++ Assets/Scripts/VendingMachine.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VendingMachine : MonoBehaviour
+{
+    #region variables
+    public bool infinitDrinks;
+
+    public GameObject energyDrink;
+
+    bool hasPressed = false;
+    bool playerIsIn;
+    #endregion
+
+    private void Update()
+    {
+        if (playerIsIn)
+        {
+            if (Input.GetKeyDown(FindObjectOfType<PlayerController>().interactKey))
+            {
+                Instantiate(energyDrink,
+                    new Vector3(transform.position.x, transform.position.y - 3, 0f),
+                    Quaternion.identity);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerController player = collision.GetComponent<PlayerController>();
+        if (player != null)
+        {
+            if (!hasPressed || infinitDrinks)
+            {
+                playerIsIn = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        PlayerController player = collision.GetComponent<PlayerController>();
+        if (player != null)
+        {
+            playerIsIn = false;
+        }
+    }
+}
