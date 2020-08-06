1cfa953edd4a1ada03c18061f02ca337900eff46
Embersands game Studios <eitan.levy.159@gmail.com>
2020-08-06 10:16:10 +0300

    Add enemy movement

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed;
+    public bool vertical;
+    public float changeTime = 3.0f;
+
+    float timer;
+    int direction = 1;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        timer = changeTime;
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rb.position;
+
+        if (vertical)
+        {
+            position.y = position.y + Time.deltaTime * speed * direction; ;
+        }
+        else
+        {
+            position.x = position.x + Time.deltaTime * speed * direction; ;
+        }
+
+        rb.MovePosition(position);
+    }
+}
